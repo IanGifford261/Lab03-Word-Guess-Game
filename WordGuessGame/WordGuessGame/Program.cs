e235bc58572c5f88dd99756cf7b7e0895873c993
iangifford <lightyagamie10@gmail.com>
2019-03-23 12:50:38 -0700

    I dont know

---
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WordGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void AddWords(string word)
+        {
+            string filePath = "./addWords.txt";
+            string[] wordsArray = new string[] { word };
+            System.IO.File.AppendAllLines(filePath, wordsArray);
+        }
+    }
+}
